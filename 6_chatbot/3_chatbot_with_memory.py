2dc503c1c587e24d640451087bc9bfdef95824f1
Punit <3033.punit@gmail.com>
2026-08-16 00:41:11 +0530

    ChatBot with memory and checkpointers

---
+++ 6_chatbot/3_chatbot_with_memory.py
@@ -0,0 +1,72 @@
+from typing import TypedDict, Annotated
+from langgraph.graph import StateGraph, add_messages, END
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.tools import TavilySearchResults
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import ToolNode
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o")
+memory = MemorySaver()
+
+tool = TavilySearchResults(max_results=2)
+tools = [tool]
+
+llm_with_tools = llm.bind_tools(tools=tools)
+
+class BasicChatState(TypedDict):
+    messages:Annotated[list, add_messages]
+
+def chat_bot(state:BasicChatState):
+    return{
+        "messages":[llm_with_tools.invoke(state["messages"])]
+    }
+
+def tools_router(state:BasicChatState):
+    last_message = state["messages"][-1]
+
+    if(hasattr(last_message,"tool_calls") & len(last_message.tool_calls)>0):
+        return TOOL_CALL
+    else:
+        return END
+    
+
+graph = StateGraph(BasicChatState)
+
+CHAT_BOT = "chat_bot"
+TOOL_CALL = "tool_node"
+
+tool_node = ToolNode(tools=tools)
+
+graph.add_node(CHAT_BOT,chat_bot)
+graph.add_node(TOOL_CALL,tool_node)
+
+graph.set_entry_point(CHAT_BOT)
+
+graph.add_edge(TOOL_CALL,CHAT_BOT)
+
+graph.add_conditional_edges(CHAT_BOT,tools_router,{
+    TOOL_CALL:TOOL_CALL,
+    END:END
+})
+
+
+app = graph.compile(checkpointer=memory)
+
+config = {"configurable":{
+    "thread_id":1
+}}
+
+while True :
+    user_input = input("User: ")
+    if user_input in ["EXIT","END","end","exit"]:
+        break
+    else:
+        result = app.invoke({
+            "messages":[HumanMessage(content=user_input)]
+        },config=config)
+
+    print(f"RESPONSE-----> {result} \n==========================")
